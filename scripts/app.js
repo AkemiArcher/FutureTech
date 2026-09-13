54bb8770343c1047c19814f38202f4bc46f1e17c
akemi <sergeytolkahev360@gmail.com>
2026-09-13 21:10:17 +0300

    ended video player

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 import Header from "./Header"
 import TabsCollection from "./Tabs"
+import VideoPlayerCollection from "./VideoPlayer";
 
 new Header()
 new TabsCollection()
+new VideoPlayerCollection()
